12973bdb871ee800e7e902248807a57f5933034c
AerGameChannel <63594377+AerGameChannel@users.noreply.github.com>
2020-05-06 17:15:45 +0300

    Add files via upload

---
+++ AerUtils-WWS/EventHandler.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using WW_SYSTEM;
+using WW_SYSTEM.API;
+using WW_SYSTEM.EventHandlers;
+using WW_SYSTEM.Events;
+
+namespace AerUtils
+{
+    class EventHandler : IEventHandlerAdminQuery, IEventHandlerPlayerJoin
+    {
+        public Plugin plugin;
+
+        public EventHandler(Plugin plugin)
+        {
+            this.plugin = plugin;
+        }
+        public void OnPlayerJoin(PlayerJoinEvent ev)
+        {
+            bool utilsenable = plugin.Config.GetBool("aerutils_enable", true);
+            if (!utilsenable) return;
+            bool aerutils_jbc = plugin.Config.GetBool("aerutils_jbc_enable", true);
+            if (!aerutils_jbc) return;
+
+            uint bctime = plugin.Config.GetUInt("aerutils_jbc_time", 15);
+            string bcmsg = plugin.Config.GetString("aerutils_jbc_message", "Welcome to the server!");
+            ev.Player.PersonalBroadcast(bcmsg, bctime, false);
+        }
+        public void OnAdminQuery(AdminQueryEvent ev)
+        {
+            bool utilsenable = plugin.Config.GetBool("aerutils_enable", true);
+            if (!utilsenable) return;
+            string[] array = ev.Query.Split(new char[]{' '});
+
+            if (ev.Query.ToLower().StartsWith("lo") || ev.Query.ToLower().StartsWith("lights"))
+            {
+                bool aerutils_lo = plugin.Config.GetBool("aerutils_lo_enable", true);
+                if (!aerutils_lo) return;
+                if (array.Length <= 1)
+                {
+                    ev.Output = "AerUtils_LightsOff#Usage: lo/lights <time in seconds>";
+                    ev.Successful = true;
+                    ev.Handled = true;
+                    return;
+                }
+                if (string.IsNullOrEmpty(array[1]))
+                {
+                    ev.Output = "AerUtils_LightsOff#Usage: lo/lights <time in seconds>";
+                    ev.Successful = true;
+                    ev.Handled = true;
+                    return;
+                }
+                if (array.Length > 1)
+                {
+                    try
+                    {
+                        if (!ev.Admin.IsPermitted(PlayerPermissions.FacilityManagement))
+                        {
+                            ev.Output = "AerUtils_LightsOff#Not enough permissions";
+                            ev.Successful = false;
+                            ev.Handled = true;
+                            return;
+                        }
+                        if (array.Length > 1)
+                        {
+                            if (array[1].ToLower().Contains("help"))
+                            {
+                                ev.Output = "AerUtils_LightsOff#Usage: lo <time in seconds>";
+                                ev.Successful = true;
+                                ev.Handled = true;
+                                return;
+                            }
+                            else
+                            {
+                                int lotime = int.Parse(array[1]);
+
+                                Server.Round.EnableFlickering(lotime, false);
+
+                                ev.Output = "AerUtils_LightsOff#Turned off the lights ";
+                                ev.Successful = true;
+                                ev.Handled = true;
+                                return;
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        ev.Output = "LightsOff#Error: " + ex;
+                        ev.Successful = false;
+                        ev.Handled = true;
+                        return;
+                    }
+                }
+            }
+            if (ev.Query.ToLower().StartsWith("bd"))
+            {
+                if (!utilsenable) return;
+                bool aerutils_bd = plugin.Config.GetBool("aerutils_breakdoors_enable", true);
+                if (!aerutils_bd) return;
+                if (array.Length <= 1)
+                {
+                    ev.Output = "AerUtils_BreakDoors#Usage: bd <RemoteAdmin player id>";
+                    ev.Successful = true;
+                    ev.Handled = true;
+                    return;
+                }
+                if (string.IsNullOrEmpty(array[1]))
+                {
+                    ev.Output = "AerUtils_BreakDoors#Usage: bd <RemoteAdmin player id>";
+                    ev.Successful = true;
+                    ev.Handled = true;
+                    return;
+                }
+                if (array.Length > 1)
+                {
+                    try
+                    {
+                        if (!ev.Admin.IsPermitted(PlayerPermissions.PlayersManagement))
+                        {
+                            ev.Output = "AerUtils_BreakDoors#Not enough permissions";
+                            ev.Successful = false;
+                            ev.Handled = true;
+                            return;
+                        }
+                        if (array.Length > 0)
+                        {
+                            if (array[1].ToLower().Contains("help"))
+                            {
+                                ev.Output = "AerUtils_BreakDoors#Usage: bd <RemoteAdmin player id>";
+                                ev.Successful = true;
+                                ev.Handled = true;
+                                return;
+                            }
+                            else
+                            {
+                                int id = int.Parse(array[1]);
+
+                                Player pl = Server.Round.FindPlayerWithId(id);
+                                if (pl != null && !pl.BreakDoors)
+                                {
+                                    pl.BreakDoors = true;
+
+                                    ev.Output = "AerUtils_BreakDoors#Enabled BreakDoors for player " + pl.Nick;
+                                    ev.Successful = true;
+                                    ev.Handled = true;
+                                    return;
+                                }
+                                else if (pl != null && pl.BreakDoors)
+                                {
+                                    pl.BreakDoors = false;
+
+                                    ev.Output = "AerUtils_BreakDoors#Disabled BreakDoors for player " + pl.Nick;
+                                    ev.Successful = true;
+                                    ev.Handled = true;
+                                    return;
+                                }
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        ev.Output = "AerUtils_BreakDoors#Error: " + ex;
+                        ev.Successful = false;
+                        ev.Handled = true;
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,39 @@
+# AerUtils-WWS
+An WW_SYSTEM plugin for SCP:SL, that have very big count of functions
+
+# Configs
+| Option | Type | Default Value | Description |
+| --- | --- | --- | --- |
+| aerutils_enable | Bool | true | Enables/Disables the plugin |
+
+# Join Broadcasts Config
+| Option | Type | Default Value | Description |
+| --- | --- | --- | --- |
+| aerutils_jbc_enable | Bool | true | Enables/Disables join broadcasts |
+| aerutils_jbc_message | String | Welcome to the server! | Join broadcast message |
+| aerutils_jbc_time | UInt | 15 | Join broadcast time |
+
+# LightsOff Config
+| Option | Type | Default Value | Description |
+| --- | --- | --- | --- |
+| aerutils_lo_enable| Bool | true | Enables/Disables LightsOff |
+
+# LightsOff Commands
+| Command | Description |
+| --- | --- |
+| lights | Shows usage of command |
+| lo | Shows usage of command |
+| lights help | Shows usage of command |
+| lo help | Shows usage of command |
+| lights [time in seconds] | Disables lights in the facility for N time (in seconds) |
+| lo [time in seconds] | Disables lights in the facility for N time (in seconds) |
+
+# BreakDoors Configs
+| aerutils_breakdoors_enable | Bool | true | Enables/Disabled BreakDoors |
+
+# BreakDoors Commands
+| Command | Description |
+| --- | --- |
+| bd | Shows usage of command |
+| bd help | Shows usage of command |
+| bd [RA Player id] | Enables/Disabled BreakDoors mode for player |
